bab2f987c60b55b44e0f30e81a5c3f131c08fe29
DementyevNikita <nikitos280800@gmail.com>
2024-10-21 19:07:25 +0700

    HW3-4

--- HW4/queue1.erl
+++ HW4/queue1.erl
@@ -1,6 +1,6 @@
 -module(queue1).
 -export([new/0, enqueue/2, dequeue/1, top/1, isEmpty/1]).
-
+-include_lib("eunit/include/eunit.hrl").
 -type queue() :: list().
 -spec new() -> queue().
 new() ->
@@ -27,5 +27,53 @@ top([Head| _Tail]) ->
 isEmpty(Queue) ->
         Queue =:=[].
 
+new_test() ->
+        NewQueue = new(),
+        ?assertEqual([],NewQueue),
+        ok.
+enqueue_test() ->
+        NewQueue = new(),
+        S1 = enqueue(4, NewQueue),
+        S2 = enqueue(5, S1),
+        [Top | _] = S2,
+        ?assertEqual(4,Top),
+        ok.
 
+dequeue_test() ->
+        NewQueue = new(),
+        S1 = enqueue(4, NewQueue),
+        S2 = enqueue(5, S1),
+        {Top,Bottom} = dequeue(S2),
+        ?assertEqual(4,Top),
+        ?assertEqual([5],Bottom),
+        ok.
+
+dequeue1_test() ->
+        NewQueue = new(),
+	{Top,Bottom} = dequeue(NewQueue),
+        ?assertEqual({error, empty_queue}, {Top,Bottom}),
+        ok.
+
+top_test() ->
+        NewQueue = new(),
+        S1 = enqueue(4, NewQueue),
+        S2 = enqueue(5, S1),
+        ?assertEqual(4, top(S2)),
+        ok.
+
+top1_test() ->
+        NewQueue = new(),
+        ?assertEqual({error, empty_queue},top(NewQueue)),
+        ok.
+
+isEmpty_test() ->
+        NewQueue = new(),
+        ?assertEqual(true,isEmpty(NewQueue)),
+        ok.
+
+isEmpty1_test() ->
+        NewQueue = new(),
+        S1 = enqueue(7,NewQueue),
+        ?assertEqual(false,isEmpty(S1)),
+        ok.
 
--- HW4/stek.erl
+++ HW4/stek.erl
@@ -1,6 +1,6 @@
 -module(stek).
 -export([new/0, push/2, pop/1, top/1, isEmpty/1]).
-
+-include_lib("eunit/include/eunit.hrl").
 -type stack() :: list(). 
 -spec new() -> stack().
 new() ->
@@ -21,10 +21,59 @@ pop([Head|Tail]) ->
 top([]) ->
 	{error, empty_stack};
 top([Head| _Tail]) -> 
-	{Head}.
+	Head.
 
 -spec isEmpty(Stack::list()) -> boolean().
 isEmpty(Stack) ->
 	Stack =:=[].
 
+new_test() ->
+	NewStack = new(),
+        ?assertEqual([],NewStack),
+        ok.
+push_test() ->
+	NewStack = new(),
+        S1 = push(4, NewStack),
+        S2 = push(5, S1),
+	[Top | _] = S2,
+        ?assertEqual(5,Top),
+        ok.
+
+pop_test() ->
+	NewStack = new(),
+	S1 = push(4, NewStack),
+	S2 = push(5, S1),
+	{Top,Bottom} = pop(S2),
+	?assertEqual(5,Top),
+	?assertEqual([4],Bottom),
+	ok.
+
+pop1_test() ->
+        NewStack = new(),
+        {Top,Bottom} = pop(NewStack),
+        ?assertEqual({error, empty_stack},{Top,Bottom}),
+        ok.
+
+top_test() ->
+        NewStack = new(),
+        S1 = push(4, NewStack),
+        S2 = push(5, S1),
+        ?assertEqual(5, top(S2)),
+        ok.
+
+top1_test() ->
+        NewStack = new(),
+        ?assertEqual({error, empty_stack},top(NewStack)),
+        ok.
+
+isEmpty_test() -> 
+	NewStack = new(),
+	?assertEqual(true,isEmpty(NewStack)),
+        ok.
+
+isEmpty1_test() ->
+        NewStack = new(),
+	S1 = push(7,NewStack),
+        ?assertEqual(false,isEmpty(S1)),
+        ok.
 
