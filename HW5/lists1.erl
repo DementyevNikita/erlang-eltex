74421526a4a1f6ff2695ef51e4cf624a5e9a131f
DementyevNikita <nikitos280800@gmail.com>
2024-11-20 20:19:09 +0700

    Homework5

--- HW5/lists1.erl
+++ HW5/lists1.erl
@@ -1,5 +1,7 @@
 -module(lists1).
--export([users/0, users1/0, users2/0, users3/0, spisok/0, spisok1/0, spisok2/0,users_acc/2]).
+-export([users/0, users1/0, users2/0, users3/0, spisok/0, spisok1/0, spisok2/0]).
+
+-include_lib("eunit/include/eunit.hrl").
 
 users() ->
 	[{user,1,"Sasha", male, 23},
@@ -8,14 +10,15 @@ users() ->
 	 {user,4,"Bob", male, 18},
 	 {user,5,"Lera", female, 21}].
 
-
-
+-spec users1() -> list(gender).
 users1() ->
 	[User || {user, _,_, Gender, _} = User <- users(), Gender =:=male].
 
+-spec users2() -> list(name).
 users2() ->
 	[Name || {user, _,Name,_, _} <- users()].
 
+-spec users3() -> list(id).
 users3() ->
 	[User || {user, Id,_,_, _} = User <- users(),Id > 2 andalso Id < 5 ].
 
@@ -24,8 +27,21 @@ spisok() ->
 spisok1() ->
         [0,4,6,2,1,8,0,5,5].
 
-
+-spec spisok2() -> list.
 spisok2()->
        [X * Y || X<-spisok(), X > 4, Y <- spisok(), Y < 4].
 
+users1_test() ->
+        ?assertEqual([{user,1,"Sasha",male,23},
+ {user,2,"Misha",male,27},
+ {user,4,"Bob",male,18}],users1()).
+
+users2_test() ->
+	?assertEqual(["Sasha","Misha","Lida","Bob","Lera"], users2()).
+
+users3_test() ->
+	?assertEqual([{user,3,"Lida",female,35},{user,4,"Bob",male,18}], users3()).
+
+spisok2_test() ->
+	?assertEqual([5,10,10,10,7,14,14,14,7,14,14,14,9,18,18,18], spisok2()),		ok.
 
